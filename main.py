1905387eeba2ac8c9d67ba1eb905c5245b415146
keeeal <james@keal.com.au>
2021-07-16 11:24:19 +0930

    weights and prices

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+from os import name
 from pathlib import Path
 
 import pandas as pd
@@ -20,13 +21,40 @@ def get_wins(pairs):
         for t in set(names)
     }
 
-def normalise_data(treats, pairs):
+def add_wins_to_treats(treats, pairs):
     wins = get_wins(pairs)
     wins = map(wins.get, treats['NAME'])
     treats['WINS'] = list(wins)
-    treats['PRICE'] /= max(treats['PRICE'])
-    treats = treats.drop(columns=['NAME'])
     return treats
+
+def add_weights_to_treats(treats, pairs):
+    items = [i for p in pairs for i in p]
+    names = [i.name for i in items]
+    order = list(treats['NAME'])
+    treats['WEIGHT'] = [
+        sum(i.weight for i in items if i.name == t) / names.count(t)
+        for t in order
+    ]
+    return treats
+
+def normalise_treats(treats):
+    treats = treats.drop(columns=['NAME'])
+    treats['PRICE_100G'] /= max(treats['PRICE_100G'])
+    treats['PRICE_ITEM'] /= max(treats['PRICE_ITEM'])
+    treats['WEIGHT'] /= max(treats['WEIGHT'])
+    return treats
+
+def calculate_prices(treats, pairs):
+    items = [i for p in pairs for i in p]
+    names = [i.name for i in items]
+    order = list(treats['NAME'])
+    for i in items:
+        i.price = i.weight * treats['PRICE_100G'][order.index(i.name)] / 100
+    treats['PRICE_ITEM'] = [
+        sum(i.price for i in items if i.name == t) / names.count(t)
+        for t in order
+    ]
+    return treats, pairs
 
 def print_left_right_bias(pairs):
     print('left wins:', sum(l.winner for l, r in pairs))
@@ -36,6 +64,17 @@ def print_ranking(pairs):
     wins = get_wins(pairs)
     for t in sorted(wins, key=wins.get, reverse=True):
         print(t, wins[t])
+
+def print_bill(pairs):
+    for pair in pairs:
+        for i in pair:
+            print()
+            print(f'{i.weight}g of {i.name}')
+            print(f'\t${i.price:.2f}')
+
+    print(8 * '=')
+    print(f'TOTAL:')
+    print(f'\t${sum(i.price for p in pairs for i in p):.2f}')
 
 def plot_correlation(data):
     corr = data.corr().stack().reset_index(name="correlation")
@@ -53,7 +92,7 @@ def plot_correlation(data):
 
 def plot_causation(data):
     structure = notears.from_pandas(data)
-    structure.remove_edges_below_threshold(0.45)
+    structure.remove_edges_below_threshold(0.85)
     graph = plot_structure(structure, graph_attributes={'scale': '2.0'})
     graph.draw('structure.png')
 
@@ -70,7 +109,7 @@ def plot_violin(data, x, y):
     plt.savefig(f'{x}_{y}.png')
     plt.close()
 
-def plot_scatter(data, x, y):
+def plot_linear(data, x, y):
     plot = sns.lmplot(
         x=x, y=y, data=data, palette="muted",
         scatter_kws={"s": 50, "alpha": 1}
@@ -81,25 +120,26 @@ def plot_scatter(data, x, y):
 def main():
     treats = load_treats(Path('data') / 'treats.csv')
     pairs = load_pairs(Path('data') / 'pairs')
-    data = normalise_data(treats, pairs)
+    treats, pairs = calculate_prices(treats, pairs)
+    treats = add_weights_to_treats(treats, pairs)
+    treats = add_wins_to_treats(treats, pairs)
+    treats = normalise_treats(treats)
+
+    # print_bill(pairs)
 
     # print_left_right_bias(pairs)
 
-    print_ranking(pairs)
+    # print_ranking(pairs)
 
-    plot_correlation(data)
+    plot_correlation(treats)
 
-    # plot_causation(data)
+    # plot_causation(treats)
 
-    for x in 'CHICKEN', 'HUMANFOOD':
-        plot_violin(data, x, 'WINS')
+    # for x in 'CHICKEN', 'HUMANFOOD':
+    #     plot_violin(treats, x, 'WINS')
 
-    # for x in 'HARDNESS', 'PROTEIN', 'FAT', 'PRICE':
-    #     plot_scatter(data, x, 'WINS')
-
-
-
-
+    for x in 'HARDNESS', 'PROTEIN', 'FAT', 'PRICE_ITEM', 'WEIGHT':
+        plot_linear(treats, x, 'WINS')
 
 if __name__ == '__main__':
     main()
